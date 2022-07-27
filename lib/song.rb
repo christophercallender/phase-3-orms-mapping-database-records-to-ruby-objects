e5bd2c3463f28341c26c2ddd042797ebc23a4080
Christopher Callender <christophercallender0000@gmail.com>
2022-07-27 14:56:35 -0400

    Completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,52 +1,37 @@
 class Song
-
   attr_accessor :name, :album, :id
-
   def initialize(name:, album:, id: nil)
     @id = id
     @name = name
     @album = album
   end
-
   def self.drop_table
-    sql = <<-SQL
-      DROP TABLE IF EXISTS songs
-    SQL
-
-    DB[:conn].execute(sql)
+    DB[:conn].execute("DROP TABLE IF EXISTS songs")
   end
-
   def self.create_table
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        album TEXT
-      )
-    SQL
-
-    DB[:conn].execute(sql)
+    DB[:conn].execute("CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY, name TEXT, album TEXT)")
   end
-
   def save
-    sql = <<-SQL
-      INSERT INTO songs (name, album)
-      VALUES (?, ?)
-    SQL
-
     # insert the song
-    DB[:conn].execute(sql, self.name, self.album)
-
+    DB[:conn].execute(
+      "INSERT INTO songs (name, album) VALUES ('#{self.name}', '#{self.album}')"
+    )
     # get the song ID from the database and save it to the Ruby instance
     self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
-
     # return the Ruby instance
     self
   end
-
   def self.create(name:, album:)
-    song = Song.new(name: name, album: album)
-    song.save
+    # self.new is equivalent to Song.new
+    self.new(name: name, album: album).save
   end
-
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+  def self.all
+    DB[:conn].execute("SELECT * FROM songs").map { |row| self.new_from_db(row) }
+  end
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM songs WHERE name = '#{name}' LIMIT 1").map { |row| self.new_from_db(row) }.first
+  end
 end
